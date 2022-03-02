c338b56836556f897862982a2bb105d74ce7958f
Alex Jover <alexjovermorales@gmail.com>
2022-03-02 15:31:45 +0100

    feat: use @storyblok/vue and add useStoryblok composable

--- lib/src/runtime/plugin-no-client.ts
+++ lib/src/runtime/plugin-no-client.ts
@@ -1,7 +1,7 @@
-import { StoryblokVue, apiPlugin } from "@storyblok/vue";
+import { StoryblokVue } from "@storyblok/vue";
 import { defineNuxtPlugin, useRuntimeConfig } from "#app";
 
 export default defineNuxtPlugin(({ vueApp }) => {
   const { storyblok } = useRuntimeConfig();
-  vueApp.use(StoryblokVue, { ...storyblok, use: [apiPlugin] });
+  vueApp.use(StoryblokVue, { ...storyblok });
 });
